b9035f5e4b159297dcf13e0f8ea28a79dba95f92
Mauricio <sayavedra_mau@hotmail.com>
2023-04-08 19:57:34 -0300

    En este ejercicio tenéis que crear un bucle que permita concatenar textos e imprima el resultado final por consola.

---
+++ src/Tema3/Actividad3.java
@@ -0,0 +1,15 @@
+package Tema3;
+
+public class Actividad3 {
+    public static void main(String[] args) {
+
+
+
+        String[] nombres={"Jorge","Matias","Lucas","Sofia","Julian","Alberto","Andres","Micaela","Pedro"};
+
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println(nombres[i]);
+
+        }
+    }
+}
